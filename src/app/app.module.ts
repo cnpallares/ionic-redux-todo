5b12e2747715ce8d81a18931d812d93aa8c370f0
cnpallares <carlesalbert.pallares@gmail.com>
2018-02-07 22:25:20 +0100

    Adding todos page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { SplashScreen } from "@ionic-native/splash-screen";
+import { StatusBar } from "@ionic-native/status-bar";
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { TodosPage } from "../pages/todos/todos";
+import { MyApp } from "./app.component";
 
 @NgModule({
-  declarations: [
-    MyApp,
-    HomePage
-  ],
-  imports: [
-    BrowserModule,
-    IonicModule.forRoot(MyApp)
-  ],
+  declarations: [MyApp, TodosPage],
+  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-    HomePage
-  ],
+  entryComponents: [MyApp, TodosPage],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
--- src/pages/todos/todos.ts
+++ src/pages/todos/todos.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { TodoModel } from "../../shared/todo.model";
 
 /**
  * Generated class for the TodosPage page.
@@ -10,16 +11,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-todos',
-  templateUrl: 'todos.html',
+  selector: "page-todos",
+  templateUrl: "todos.html"
 })
 export class TodosPage {
+  public todos: any[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad TodosPage');
+    console.log("ionViewDidLoad TodosPage");
+    this.todos = [
+      new TodoModel("This one an element", true, false),
+      new TodoModel("This two an element", true, false),
+      new TodoModel("This three an element", true, false)
+    ];
   }
 
+  setTodoStyles(todo: TodoModel) {
+    let styles = {
+      "text-decoration": todo.isDone ? "line-through" : "none",
+      "font-weight": todo.isImportant ? "font-weight" : "600"
+    };
+    return styles;
+  }
+
+  toggleTodo(todo: TodoModel) {
+    return (todo.isDone = !todo.isDone);
+  }
 }
