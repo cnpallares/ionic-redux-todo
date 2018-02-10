2df98c9c7709c8a7b085e5646a3665048da692c5
cnpallares <carlesalbert.pallares@gmail.com>
2018-02-10 16:30:22 +0100

    AddinG HTTP module to use http from angular package

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
+import { TodoService } from "./../shared/todo-service";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { BrowserModule } from "@angular/platform-browser";
 import { ErrorHandler, NgModule } from "@angular/core";
 import { SplashScreen } from "@ionic-native/splash-screen";
 import { StatusBar } from "@ionic-native/status-bar";
+import { HttpClientModule } from "@angular/common/http";
+import { HttpModule } from "@angular/http";
 
 import { AddTaskModalPage } from "./../pages/add-task-modal/add-task-modal";
 import { TodosPage } from "../pages/todos/todos";
@@ -10,13 +13,19 @@ import { MyApp } from "./app.component";
 
 @NgModule({
   declarations: [MyApp, TodosPage, AddTaskModalPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    HttpModule,
+    HttpClientModule
+  ],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, TodosPage, AddTaskModalPage],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    TodoService
   ]
 })
 export class AppModule {}
